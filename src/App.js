ad9476f7750c8f84c65d75d4abf1979c5609196f
Sander Weiss <weisssander@gmail.com>
2023-03-24 23:30:02 -0400

    title screen working + font added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css'
 import Navigation from './screens/navigationBar/NavigationBar'
+import Header from './screens/header/Header';
 
 function App() {
   return (
-    <>
+    <div className='App'>
       <Navigation />
-    </>
+      <Header />
+    </div>
   )
 }
 
---
+++ src/screens/header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Typed from 'react-typed';
+import './header.css'
+
+export default function Header() {
+  return (
+    <div className="main-info">
+      <h1>Front End Developer</h1>
+      <Typed
+        strings={[
+            'Who loves working with React',
+            'Also CSS, SASS',
+            'Learning PHP and Typescript aswell'
+            ]}
+            typeSpeed={40}
+            backSpeed={50}
+            loop
+      />
+    </div>
+  )
+}
